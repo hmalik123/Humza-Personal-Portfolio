ea9999483c8687e9c1d7244dbe2bc4d8f52dae88
HMalik <hmalik-@hotmail.com>
2024-12-30 17:09:18 +0000

    About finished

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,44 +1,19 @@
 import React from "react";
-
 import styles from "./About.module.css";
-import { getImageUrl } from "../../utils";
 
 export const About = () => {
   return (
     <section className={styles.container} id="about">
-      <h2 className={styles.title}>About</h2>
+      <h2 className={styles.title}>About Me</h2>
       <div className={styles.content}>
         <ul className={styles.aboutItems}>
           <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
             <div className={styles.aboutItemText}>
-              <h3>Frontend</h3>
-              <br></br>
               <p>
-                I've worked with React, Vue.js and also worked on various project
-                using just pure html, CSS and JavaScript capabilites
-              </p>
-            </div>
-          </li>
-          <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
-            <div className={styles.aboutItemText}>
-              <h3>Backend</h3>
-              <br></br>
-              <p>
-                I have experience using Springboot for Java backend applications and
-                also using both flask and fastapi for Python backend apps
-              </p>
-            </div>
-          </li>
-          <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
-            <div className={styles.aboutItemText}>
-              <h3>Embedded Software Engineering</h3>
-              <br></br>
-              <p>
-                I've recently been learning electronics and embedded coding working with the Arduino 
-                and ESP322 Microcontrollers
+                I'm a BSC computer science graduate from Brunel University London, and have since been working with full-stack 
+                technologies such as React for frontend and Spring Boot, Flask for backend projects using Java and Python.
+                I've recently also developed an interest in embedded software engineering and have been working with the Arduino and ESP32 microcontrollers
+                for IoT creations using C++. I'm very passionate about the tech industry and hope to make some positive changes in the world with my skills!
               </p>
             </div>
           </li>
