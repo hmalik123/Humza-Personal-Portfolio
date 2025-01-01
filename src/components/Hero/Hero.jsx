866e9206a94d4c7c38b8174493785ba4032e25ae
HMalik <hmalik-@hotmail.com>
2025-01-01 05:43:06 +0000

    improved hero css

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,7 +5,8 @@ import ArduinoModel from "../3Dmodel/ArduinoModel";
 export const Hero = () => {
   return (
     <section className={styles.container}>
-      <div className={styles.content}>
+      <div className={styles.contentWrapper}>
+        {/* Left Side: SVG Icon */}
         <div className={styles.scrollWrapper}>
           <svg
             className={styles.scrollIcon}
@@ -24,7 +25,9 @@ export const Hero = () => {
             />
           </svg>
         </div>
-        <div>
+
+        {/* Right Side: Text Content */}
+        <div className={styles.textContent}>
           <h1 className={styles.title}>
             Hi, I'm <span className={styles.highlight}>Humza</span>
           </h1>
@@ -34,6 +37,8 @@ export const Hero = () => {
           </p>
         </div>
       </div>
+
+      {/* 3D Model Section */}
       <div className={styles.threeDModelWrapper}>
         <ArduinoModel />
       </div>
